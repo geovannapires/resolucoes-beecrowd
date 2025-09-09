5723862f1cfe304fe6a855485bfa2aaf1a9e2550
Geo <135927652+geovannapires@users.noreply.github.com>
2025-09-08 21:55:33 -0300

    Create 1005.c

---
+++ C/1005.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    double A,B,MEDIA;
+    
+    scanf("%lf\n%lf", &A, &B);
+
+    MEDIA= (3.5 * A + 7.5 * B)/11.0;
+    printf("MEDIA = %.5lf\n", MEDIA);
+    return 0;
+}
